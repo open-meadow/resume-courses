44afebb1e148fd4fc9d76b9d7a51f16aef1ba42c
open-meadow <rohan.varughese@protonmail.com>
2023-07-03 17:49:25 -0400

    Style buttons with css

--- src/app/components/BigButtons.tsx
+++ src/app/components/BigButtons.tsx
@@ -4,18 +4,18 @@ export default function BigButtons() {
   return (
     <div className="bigButtons">
       <div className="bigButtons--single">
-        <h1>Resume and Cover Letter</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non diam. Bibendum neque egestas congue quisque egestas diam. Nec nam aliquam sem et tortor consequat id porta nibh. Id diam maecenas ultricies mi eget mauris pharetra. Elementum nibh tellus molestie nunc non blandit. Mi eget mauris pharetra et ultrices neque. Aenean vel elit scelerisque mauris pellentesque. Id diam maecenas ultricies mi eget mauris pharetra et. Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna. Tempor orci dapibus ultrices in. Libero justo laoreet sit amet cursus sit.</p>
+        <h1 className="title-3">Resume and Cover Letter</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non diam. Bibendum neque egestas congue quisque egestas diam. Nec nam aliquam sem et tortor consequat id porta nibh.</p>
         <button>Learn More</button>
       </div>
-      <div>
+      <div className="bigButtons--single">
         <h1>Online Courses</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non diam. Bibendum neque egestas congue quisque egestas diam. Nec nam aliquam sem et tortor consequat id porta nibh. Id diam maecenas ultricies mi eget mauris pharetra. Elementum nibh tellus molestie nunc non blandit. Mi eget mauris pharetra et ultrices neque. Aenean vel elit scelerisque mauris pellentesque. Id diam maecenas ultricies mi eget mauris pharetra et. Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna. Tempor orci dapibus ultrices in. Libero justo laoreet sit amet cursus sit.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non diam. Bibendum neque egestas congue quisque egestas diam. Nec nam aliquam sem et tortor consequat id porta nibh.</p>
         <button>Learn More</button>
       </div>
-      <div>
+      <div className="bigButtons--single">
         <h1>College Essays</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non diam. Bibendum neque egestas congue quisque egestas diam. Nec nam aliquam sem et tortor consequat id porta nibh. Id diam maecenas ultricies mi eget mauris pharetra. Elementum nibh tellus molestie nunc non blandit. Mi eget mauris pharetra et ultrices neque. Aenean vel elit scelerisque mauris pellentesque. Id diam maecenas ultricies mi eget mauris pharetra et. Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna. Tempor orci dapibus ultrices in. Libero justo laoreet sit amet cursus sit.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna fermentum iaculis eu non diam. Bibendum neque egestas congue quisque egestas diam. Nec nam aliquam sem et tortor consequat id porta nibh.</p>
         <button>Learn More</button>
       </div>
     </div>
--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
 
 export default function Nav() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-white p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-white p-8">
       <div className="flex items-center flex-shrink-0 text-black mr-6">
         <span className="font-semibold text-xl tracking-light">
           Babayev Consulting
@@ -11,23 +10,23 @@ export default function Nav() {
       </div>
 
       <div className="w-full block lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
+        <div className="text-md lg:flex-grow">
           <a
             href="#Resume"
-            className="block mt-4 lg:inline-block lg:mt-0 text-[#2f7cc9] hover:text-cyan-600 mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 font-bold text-[#064788] hover:text-cyan-600 mr-4"
           >
             Resume
           </a>
           <a
             href="#Online Courses"
-            className="block mt-4 lg:inline-block lg:mt-0 text-[#2f7cc9] hover:text-cyan-600 mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 font-bold text-[#064788] hover:text-cyan-600 mr-4"
           >
             Online Courses
           </a>
           <a
             href="#Essay"
             hrefLang="#Essay"
-            className="block mt-4 lg:inline-block lg:mt-0 text-[#2f7cc9] hover:text-cyan-600 mr-4"
+            className="block mt-4 lg:inline-block lg:mt-0 font-bold text-[#064788] hover:text-cyan-600 mr-4"
           >
             Essay
           </a>
