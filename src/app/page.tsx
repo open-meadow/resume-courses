5b33b1952e656e9494c1ebb15fa7d113baff56dd
open-meadow <rohan.varughese@protonmail.com>
2023-07-02 22:08:03 -0400

    Initialize BigButtons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Nav from "./components/Navigation";
 import Middle from "./components/Middle";
+import BigButtons from "./components/BigButtons";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Middle/>
+      <BigButtons/>
     </>
   );
 }
