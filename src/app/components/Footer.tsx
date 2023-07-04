adb36d3cb315207c9fe4caeb165bcf11b70ecc01
open-meadow <rohan.varughese@protonmail.com>
2023-07-04 17:05:39 -0400

    Style footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,13 +2,20 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="flex flex-column justify-evenly gap-x-10">
+      <div>
+        <h1>Babayev Consultation</h1>
+      </div>
       <ul>
         <li>Resume</li>
         <li>Cover Letter</li>
         <li>Online Courses</li>
         <li>Essay</li>
       </ul>
+      <div>
+        <h1>Contact Us</h1>
+        <p>Email</p>
+      </div>
     </footer>
   );
 }
