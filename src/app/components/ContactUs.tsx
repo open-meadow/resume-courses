c3b2cde0f5b1cc341a3ace3bddd7dde20b8eda08
open-meadow <rohan.varughese@protonmail.com>
2023-07-10 17:54:05 -0400

    Create form

--- src/app/components/ContactUs.tsx
+++ src/app/components/ContactUs.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 export default function ContactUs() {
-    return(
-        <div>Hello World</div>
-    );
+  return (
+    <div className="p-2 py-5 flex flex-col items-center">
+      <h1 className="font-bold text-4xl text-center text-[#064788]">Contact Us</h1>
+      <div className="grid grid-cols-1 gap-6 w-3/4">
+        <label className="block">
+          <span>Name</span>
+          <input
+            type="text"
+            className="mt-1 block w-full rounded-xl"
+            placeholder="Enter your name"
+          />
+        </label>
+        <label className="block">
+          <span>Email</span>
+          <input
+            type="email"
+            className="mt-1 block w-full rounded-xl"
+            placeholder="Enter your e-mail"
+          />
+        </label>
+        <label className="block">
+          <span>Message</span>
+          <textarea
+            className="mt-1 block w-full h-40 rounded-xl"
+            placeholder="What services are you looking for?"
+          />
+        </label>
+      </div>
+    </div>
+  );
 }
--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Testimonials() {
   return (
-    <div className="testimonials flex flex-col items-center p-3">
+    <div className="bg-slate-800 text-white overflow-hidden flex flex-col items-center p-3">
       <h1 className="text-4xl font-bold">Testimonials</h1>
-      <div className="overflow-hidden flex p-10 w-screen justify-evenly">
+      <div className="flex p-10 w-screen justify-evenly">
         <img src="https://placehold.co/300x300" className="rounded-full" />
         <div className="flex flex-col justify-center md-2">
           <h2 className="text-3xl font-semibold">
