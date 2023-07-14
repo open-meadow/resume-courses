e445662dbcf713459a8743d0c03b4a3529f22b75
open-meadow <rohan.varughese@protonmail.com>
2023-07-14 16:35:30 -0400

    Fix minor bug

---
+++ src/app/online-course copy/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ContactUs from "../components/ContactUs";
+import Footer from "../components/Footer";
+import Middle from "../components/Middle";
+import Nav from "../components/Navigation";
+
+export default function Resume() {
+  return (
+    <>
+      <Nav />
+      <Middle/>
+      <ContactUs/>
+      <Footer/>
+    </>
+  );
+}
